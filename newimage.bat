96b0b7daf15316450e654741c1f5c02f7101e536
drooo2 <andrew.zeng01@gmail.com>
2020-05-13 17:08:53 -0400

    made changes

--- newimage.bat
+++ newimage.bat
@@ -1,4 +1,4 @@
 python C:\Users\drewm\Desktop\Vew\src\download_images.py
 python C:\Users\drewm\Desktop\Vew\src\compress.py %*
 timeout /t 4 /nobreak
-Powershell.exe -executionpolicy remotesigned -File  C:\Users\drewm\Desktop\Vew\Art\pushtogit.sh
+"C:\Program Files\Git\bin\sh.exe" --login -i -c  C:\Users\drewm\Desktop\Vew\Art\pushtogit.sh
