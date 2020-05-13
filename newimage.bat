3148d946071d775896f84e008847ee3889f4e65c
drooo2 <andrew.zeng01@gmail.com>
2020-05-13 15:07:32 -0400

    New image download system

--- newimage.bat
+++ newimage.bat
@@ -1,3 +1,4 @@
+python C:\Users\drewm\Desktop\Vew\src\download_images.py
 python C:\Users\drewm\Desktop\Vew\src\compress.py %*
 timeout /t 4 /nobreak
 Powershell.exe -executionpolicy remotesigned -File  C:\Users\drewm\Desktop\Vew\Art\pushtogit.sh
--- pushtogit.sh
+++ pushtogit.sh
@@ -1,5 +1,4 @@
 cd C:\Users\drewm\Desktop\Vew\Art\AllArt
-git checkout master
 git add .
 git commit -am "made changes"
-git push --all --force
+git push master --all --force
