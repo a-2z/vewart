890afb1dc615a0aef0c4f7b90199d9dd2f121fc4
drooo2 <andrew.zeng01@gmail.com>
2020-05-13 23:28:33 -0400

    made changes

--- newimage.bat
+++ newimage.bat
@@ -1,4 +1,2 @@
-python C:\Users\drewm\Desktop\Vew\src\download_images.py
-python C:\Users\drewm\Desktop\Vew\src\compress.py %*
-timeout /t 4 /nobreak
-"C:\Program Files\Git\bin\sh.exe" --login -i -c  C:\Users\drewm\Desktop\Vew\Art\pushtogit.sh
+python %~dp0\..\src\download_images.py
+python %~dp0\..\src\compress.py %*
